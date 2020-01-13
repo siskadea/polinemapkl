35e9a08227bb8f19e489f1563d8e365a02a18648
siskadea <siskhadhea@gmail.com>
2020-01-13 10:18:02 +0700

    edit

--- arduino-polinema/tampilkan.php
+++ arduino-polinema/tampilkan.php
@@ -6,6 +6,7 @@ $today = date('Y-m-d');
 $yesterday = date('Y-m-d');
 
 $result = $koneksi->query("SELECT p.id_produksi, p.waktu, s.lokasi, s.ket FROM produksi p INNER JOIN sensor s ON p.id_sensor=s.id_sensor ORDER BY waktu ASC");
+
 $shift1 = $koneksi->query("SELECT count(*) FROM produksi WHERE TIME(waktu)>='07:00:01' and TIME(waktu)<='15:00:00' AND DATE(waktu)='$today'");
 $data1 = $shift1->fetch_row();
 echo $data1[0];
@@ -13,18 +14,18 @@ echo $data1[0];
 //echo $jumlah;
 ?>
 <br>
-
+<p>siang</p>
 <?php
 $shift2 = $koneksi->query("SELECT count(*) FROM produksi WHERE TIME(waktu)>='15:00:01' and TIME(waktu)<='23:00:00' AND DATE(waktu)='$today'");
 $data2 = $shift2->fetch_row();
 echo $data2[0];
 ?>
 <br>
-
+<p> malam </p>
 <?php
-//$shift3 = $koneksi->query("SELECT count(*) FROM produksi WHERE DATETIME(waktu)>='2020-01-10 23:00:01' and DATETIME(waktu)<='2020-01-11 07:00:00'");
-//$data3 = $shift3->fetch_row();
-//echo $data3[0];
+$shift3 = $koneksi->query("SELECT count(*) FROM produksi WHERE DATE_FORMAT(waktu)>='%Y-%m-%d 23:00:01' and DATE_FORMAT(waktu)<='%Y-%m-%d 07:00:00'");
+// $data3 = $shift3->fetch_row();
+echo $shift3;
 ?>
 <br>
 
@@ -41,7 +42,7 @@ $data4 = $harian->fetch_row();
 echo $data4[0];
 ?>
 <br>
-
+<p></p>
 <?php
 $bulanan = $koneksi->query("SELECT count(*) FROM produksi WHERE MONTH(waktu) = MONTH(CURRENT_DATE())");
 $data5 = $bulanan->fetch_row();
